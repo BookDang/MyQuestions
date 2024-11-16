254fdf00f88c08dcfb25db11d22fe4e7ee3ec605
ChiSach <sachdang.dev@gmail.com>
2024-11-16 12:48:42 +0700

    Add health check and TypeORM integration for MySQL in NestJS application

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from '@/app.controller'
+import { AppService } from '@/app.service'
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql-my-questions',
+      port: 3306,
+      username: 'root',
+      password: 'mysecretpassword',
+      database: 'my_questions',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      logging: true,
+    }),
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
